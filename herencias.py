4cb15d0f32a043d6399f8d939bc53dc5595c0c3e
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-06 22:39:50 +0200

    cambios

--- herencias.py
+++ herencias.py
@@ -19,3 +19,39 @@ class Camioneta(Coche):
         self.carga = carga
     def __str__(self):
         return super().__str__() + ", {} kg".format(self.carga)
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+    def __str__(self):
+        return super().__str__() + ", {}".format(self.tipo)
+
+class Motocicleta(Bicicleta):
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+        super().__init__(color, ruedas, tipo)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    def __str__(self):
+        return super().__str__() + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
+
+
+vehiculos=[Coche("rojo", 4, 150, 1200),Camioneta("blanco", 4, 100, 2000, 500),Bicicleta("azul", 2, "urbana"), Motocicleta('negra', 2, 'deportiva', 200, 600)]
+
+def catalogar(vehiculos):
+    for v in vehiculos:
+        print('{} {}'.format(type(v).__name__, v))
+catalogar(vehiculos)
+
+def catalogar2(vehículos,ruedas=None):
+    if ruedas != None:
+        contador=0
+        for v in vehículos:
+            if v.ruedas == ruedas:
+                contador+=1
+        print("Hay {} vehículos con {} ruedas".format(contador,ruedas))
+        print('--------------------------------------------------------')
+    else:
+        if v.ruedas == ruedas:
+            print('{} {}'.format(type(v).__name__, v))
+catalogar2(vehiculos,0)
