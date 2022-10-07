c9fc3dc3ab5bac6849f8af14187fbcac5078ece2
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-07 16:59:15 +0200

    cambios

--- Ejercicio_1/crear.py
+++ Ejercicio_1/crear.py
@@ -1,3 +1,13 @@
 import alumno
 import nota
 
+
+class AlumnoNota(Alumno, Nota):
+    def __init__(self, nombre, nota):
+        alumno.Alumno.__init__(self, nombre)
+        nota.Nota.__init__(self, nota)
+    def __str__(self):
+        return alumno.Alumno.__str__(self) + ", " + Nota.__str__(self) + ", " + Nota.aprobado_supendido(self)
+
+
+
