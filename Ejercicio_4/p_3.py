c5dc9dfdcced0c8ebbcecbf787ab1910f3d92378
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-07 17:26:45 +0200

    cambios

--- Ejercicio_4/p_3.py
+++ Ejercicio_4/p_3.py
@@ -2,6 +2,18 @@ def lista2():
     try:
         paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
         p= input('introduca pais:')
-        if 
+        if p in paises:
+            print('el idioma es:',paises[p])
+        else:
+            print('no se encuentra el pais')
+            lista2()
+    except ValueError:
+        print("solo se permiten numeros")
+        lista2()
+    except:
+        print("error desconocido")
+        lista2()
+        
+
 
 
