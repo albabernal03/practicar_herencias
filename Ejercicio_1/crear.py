5727f26ae68e7d88d30369719e1a45908289b780
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-07 17:06:41 +0200

    cambios

--- Ejercicio_1/crear.py
+++ Ejercicio_1/crear.py
@@ -1,13 +1,18 @@
 import alumno
-import nota
+import calificacion
 
 
-class AlumnoNota(alumno.Alumno, nota.Nota):
+class AlumnoNota(alumno.Alumno, calificacion.Nota):
     def __init__(self, nombre, nota):
         alumno.Alumno.__init__(self, nombre)
-        nota.Nota.__init__(self, nota)
+        calificacion.Nota.__init__(self, nota)
     def __str__(self):
-        return alumno.Alumno.__str__(self) + ", " + Nota.__str__(self) + ", " + Nota.aprobado_supendido(self)
+        return alumno.Alumno.__str__(self) + ", " + calificacion.Nota.__str__(self) + ", " + calificacion.Nota.aprobado_supendido(self)
+
+if __name__ == "__main__":
+    alumno_nota = AlumnoNota("Juan", 7)
+    print(alumno_nota)
+    
 
 
 
--- Ejercicio_1/menu.py
+++ Ejercicio_1/menu.py
@@ -1,5 +1,21 @@
-import alumno
 import crear
 
 def iniciar():
+    print("Bienvenido al menu")
+    print('[1]. Crear alumno y nota')
+    print('[2]. Salir')
+
+    opcion = input("Seleccione una opcion: ")
+    if opcion == '1':
+        crear.AlumnoNota()
+    elif opcion == '2':
+        print("Saliendo...")
+        exit()
+    else:
+        print("Opcion invalida")
+        iniciar()
+        
+
+
+
     
