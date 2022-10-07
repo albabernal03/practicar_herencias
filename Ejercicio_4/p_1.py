3c2a0ee6bcf7e5c51faedfdd0e874af886693e64
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-07 17:11:21 +0200

    creamos funcion que coja error de dividir entre 0

---
+++ Ejercicio_4/p_1.py
@@ -0,0 +1,18 @@
+#crea una funcion que capture error de dividir entre cero
+
+def dividir():
+    try:
+        dividendo = int(input("Ingrese el dividendo: "))
+        divisor = int(input("Ingrese el divisor: "))
+        resultado = dividendo / divisor
+        print("El resultado es: ", resultado)
+    except ZeroDivisionError:
+        print("No se puede dividir entre cero")
+        dividir()
+    except ValueError:
+        print("Solo se permiten numeros")
+        dividir()
+    except:
+        print("Error desconocido")
+        dividir()
+        
