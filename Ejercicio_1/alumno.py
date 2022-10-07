7211365fd44481d7596d4c4b54f87f31c80cf64d
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-07 16:53:27 +0200

    probamos que la funcion creada funciona

--- Ejercicio_1/alumno.py
+++ Ejercicio_1/alumno.py
@@ -1,8 +1,31 @@
 class Alumno:
-    def __init__(self, nombre, nota):
+    def __init__(self, nombre):
         self.nombre = nombre
+    def __str__(self):
+        return'nombre: {}'.format(self.nombre)
+
+class Nota:
+    def __init__(self, nota):
         self.nota = nota
     def __str__(self):
-        return "nombre {}, nota {}".format(self.nombre, self.nota)
+        return "nota {}".format(self.nota)
+    
+    def aprobado_supendido(self):
+        if self.nota >= 5:
+            return "aprobado"
+        else:
+            return "suspendido"
 
-        
+class AlumnoNota(Alumno, Nota):
+    def __init__(self, nombre, nota):
+        Alumno.__init__(self, nombre)
+        Nota.__init__(self, nota)
+    def __str__(self):
+        return Alumno.__str__(self) + ", " + Nota.__str__(self) + ", " + Nota.aprobado_supendido(self)
+
+if __name__ == "__main__":
+    alumno = AlumnoNota("Juan", 7)
+    print(alumno)
+
+
+
